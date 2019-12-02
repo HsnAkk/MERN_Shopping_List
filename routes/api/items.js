45b4cc7c84070c16636a49e0e8ea94253af95b6e
HsnAkk <hasan.akkas@web.de>
2019-12-02 15:27:15 +0100

    first commit

---
+++ routes/api/items.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+
+
+// Item model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc get all Items
+// @access Public
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: -1})                // to sort in descending mode
+        .then(items => res.json(items))
+});
+
+// @route POST api/items
+// @desc create a post
+// @access Private
+router.post('/', auth, (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save()
+        .then( item => res.json(item));
+});
+
+// @route DELETE api/items
+// @desc delete  a item
+// @access Private
+router.delete('/:id', auth, (req, res) => {
+    Item.findById(req.params.id)
+        .then( item => item.remove().then( () => res.json({ success: true})))
+        .catch( err => res.status(404).json({ success: false }));
+});
+
+
+
+module.exports = router;
